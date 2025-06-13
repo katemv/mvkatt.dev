c43e708e2a792409bf4e9918d5ef547184c7459d
mvkatt <mvkatt@gmail.com>
2025-06-13 15:38:42 -0600

    fix ga

--- src/components/common/GoogleAnalytics.tsx
+++ src/components/common/GoogleAnalytics.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { pageview, GA_TRACKING_ID } from "@/lib/gtag";
 import Script from "next/script";
 
-export default function GoogleAnalytics() {
+function GoogleAnalyticsInner() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
@@ -42,4 +42,12 @@ export default function GoogleAnalytics() {
             />
         </>
     );
-} 
+}
+
+export default function GoogleAnalyticsWrapper() {
+    return (
+        <Suspense fallback={null}>
+            <GoogleAnalyticsInner />
+        </Suspense>
+    );
+}
